e0a907db84885544ab14444b16c2f10a49262d20
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-15 23:10:19 +0100

    add main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# import subprocess
+# import sys
+
+import streamlit as st
+
+from gbs.core.gbs import GBS
+
+# header image
+img = "./assets/img.png"
+st.image(img)
+
+
+# title
+st.title("Global Biodiversity Score")
+st.write(
+    """
+GBS enables companies and financial institutions to measure their impact on biodiversity and integrate this information into their operational management policy and decision-making strategy. 
+In this way, they can align themselves with international objectives and constantly evolving regulations..
+"""
+)
