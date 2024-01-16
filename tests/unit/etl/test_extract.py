6f3f5cfda6569a8bbd99575f586d17efe86fbd48
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-16 03:06:00 +0100

    update tests

--- tests/unit/etl/test_extract.py
+++ tests/unit/etl/test_extract.py
@@ -81,24 +81,40 @@ class TestExtract:
     def test_get_production(self, extract):
         """Test the get_production method of the Extract class."""
 
-        extract.get_production()
+        # extract.get_production()
+
+        # assert os.path.exists(
+        #     os.path.join(
+        #         extract.base,
+        #         extract.folder,
+        #         extract.subfolder,
+        #         "production",
+        #     )
+        # )
+
+        pass
+
+    def test_curl_production(self, extract):
+        """Test the _curl_production method of the Extract class."""
+
+        extract._curl_production()
 
         assert os.path.exists(
             os.path.join(
                 extract.base,
                 extract.folder,
                 extract.subfolder,
-                "production",
+                "production.zip",
             )
         )
 
     def test_check_folders(self, extract):
         """Test the _check_folders method of the Extract class."""
 
-        extract.check_files()
+        extract.check_files(include_production=False)
 
     @pytest.mark.parametrize("clean", [True, False])
     def test_get_all(self, extract, clean):
         """Test the get_all method of the Extract class."""
 
-        extract.get_all(clean=clean)
+        extract.get_all(clean=clean, include_production=False)
