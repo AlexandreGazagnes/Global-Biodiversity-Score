5fb3b64408ee0bdf0ec97a85207edeb1cb884a9a
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-19 14:51:42 +0100

    update .utils

--- .utils/build.sh
+++ .utils/build.sh
@@ -2,11 +2,11 @@
 
 # build docker image
 # --progress=plain for showing progress bar
-docker build -f ./Dockerfile  -t gbs:latest .
+sudo docker build -f ./Dockerfile  -t gbs:latest .
 
 # run docker image
 # -d for detached mode
-sudo docker run  -p 8501:8501 --env-file .env/.env.dev gbs:latest
+sudo docker run  -p 8501:8501 gbs:latest
 
 # if needed to run bash inside container
 # docker exec -it <container-name> /bin/bash
