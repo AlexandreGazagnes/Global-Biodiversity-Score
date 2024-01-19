f7ce7d0c5583784581ffce52af42ca50580dc27d
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-19 13:59:07 +0100

    add tests

---
+++ tests/unit/etl/test_loader.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+import pytest
+
+import pandas as pd
+
+from gbs.etl.load import Loader
+
+
+class TestLoader:
+    """Test the Loader class."""
+
+    def test_final(self):
+        """test final method of the Loader class."""
+
+        df = Loader.final()
+
+        assert isinstance(df, pd.DataFrame)
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
