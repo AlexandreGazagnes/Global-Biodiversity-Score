04d9bbd7e2a31909ba1fdbad0f78ed278d15bc48
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-19 16:18:40 +0100

    mess up with chmod

---
+++ main.py
@@ -0,0 +1,32 @@
+# import subprocess
+# import sys
+
+import streamlit as st
+
+
+from gbs.front.text import Text
+from gbs.etl.load import Loader
+from gbs.core.gbs import Gbs
+
+# header image
+img = "./.assets/img.png"
+st.image(img)
+
+
+# title
+st.title(Text.title)
+st.write(Text.descr)
+
+
+# load data
+df = Loader.final()
+
+st.subheader("Data")
+st.table(df)
+
+
+# gbs
+st.subheader("GBS")
+country = st.selectbox("country_name", df["country_name"].unique())
+if country:
+    st.table(df.loc[df["country_name"] == country])
