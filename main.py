d2f6edf5f578fbf79cfb4ae0971c41df005d29e6
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-19 17:26:35 +0100

    update main

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 # import subprocess
 # import sys
 
-# import streamlit as st
-# from gbs.front.text import Text
+import streamlit as st
+from gbs.front.text import Text
 
 
 # from gbs.etl.extract import Extract
 from gbs.core.gbs import Gbs
-from gbs.et
+from gbs.etl.loader import Loader
 
-# # header image
-# img = "./assets/img.png"
-# st.image(img)
+# df
+df = Loader.final()
+
+# header image
+img = "./.assets/img.png"
+st.image(img)
 
 
-# # title
-# st.title(Text.title)
-# st.write(Text.descr)
+# title
+st.title(Text.title)
+st.write(Text.descr)
+
+
+# data
+st.subheader("Data")
+st.table(df)
+
+# country
+st.subheader("Country")
+
+country_name = st.selectbox(
+    "Select a country",
+    df["country_name"].unique()
+    # , help="Select a country"
+)
+if country_name:
+    st.table(df[df["country_name"] == country_name])
