174e9886a5af26ee0290cda634d38e0761267caf
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-19 17:19:22 +0100

    moove utils

--- main.py
+++ main.py
@@ -5,8 +5,9 @@
 # from gbs.front.text import Text
 
 
-from gbs.etl.extract import Extract
+# from gbs.etl.extract import Extract
 from gbs.core.gbs import Gbs
+from gbs.et
 
 # # header image
 # img = "./assets/img.png"
