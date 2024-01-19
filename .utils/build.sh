45629bde4c76b90f65f6ad0f060f3a6b802a49a7
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-01-19 16:43:45 +0100

    prepare for app push

--- .utils/build.sh
+++ .utils/build.sh
@@ -6,7 +6,7 @@ docker build -f ./Dockerfile  -t gbs:latest .
 
 # run docker image
 # -d for detached mode
-sudo docker run  -p 8501:8501 --env-file .env/.env.dev gbs:latest
+sudo docker run  -p 8501:8501 gbs:latest
 
 # if needed to run bash inside container
 # docker exec -it <container-name> /bin/bash
